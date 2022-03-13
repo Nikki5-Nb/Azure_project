0c2bc5aa9b1e718c073b58180284d471a90d733a
Nikki5-Nb <66305402+Nikki5-Nb@users.noreply.github.com>
2022-03-13 18:16:56 +0100

    add variables and imports

--- Snake.py
+++ Snake.py
@@ -1,3 +1,14 @@
+from random import randrange
+from turtle import *
+
+from freegames import square, vector
+
+food = vector(0, 0)
+snake = [vector(10, 0)]
+aim = vector(0, -10)
+
+
+
 def change(x, y):
     """Change snake direction."""
     aim.x = x
